f471a35e5d4a6c5dbcf719bf6a14425a4c7dcf9f
Re-Joice <ofosuarmahrejoice@gmail.com>
2026-07-12 09:24:02 -0700

    Solve task 2: Unique addition

---
+++ python-more_data_structures/2-uniq_add.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def uniq_add(my_list=[]):
+    """Adds all unique integers in a list."""
+    unique = set(my_list)
+    total = 0
+
+    for num in unique:
+        total += num
+
+    return total
