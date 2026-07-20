c0d2ae315df76ba283995afb5aca17342887d686
Re-Joice <ofosuarmahrejoice@gmail.com>
2026-07-20 12:49:20 -0700

    Solve task 1: Square with size

---
+++ python-classes/1-square.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Defines a square."""
+
+
+class Square:
+    """Defines a square with a private size."""
+
+    def __init__(self, size):
+        """Initialize a square with a private size."""
+        self.__size = size
