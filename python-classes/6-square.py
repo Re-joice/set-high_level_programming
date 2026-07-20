355a34f84c39724d8eac93985bbcf363274cc6cf
Re-Joice <ofosuarmahrejoice@gmail.com>
2026-07-20 13:24:02 -0700

    Solve task 6: Coordinates of a square

---
+++ python-classes/6-square.py
@@ -0,0 +1,58 @@
+#!/usr/bin/python3
+"""Defines a Square class."""
+
+
+class Square:
+    """Represents a square."""
+
+    def __init__(self, size=0, position=(0, 0)):
+        """Initialize a square."""
+        self.size = size
+        self.position = position
+
+    @property
+    def size(self):
+        """Retrieve the size."""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """Set the size with validation."""
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    @property
+    def position(self):
+        """Retrieve the position."""
+        return self.__position
+
+    @position.setter
+    def position(self, value):
+        """Set the position with validation."""
+        if (not isinstance(value, tuple) or
+                len(value) != 2 or
+                not all(isinstance(i, int) for i in value) or
+                not all(i >= 0 for i in value)):
+            raise TypeError(
+                "position must be a tuple of 2 positive integers"
+            )
+        self.__position = value
+
+    def area(self):
+        """Return the area of the square."""
+        return self.__size ** 2
+
+    def my_print(self):
+        """Print the square using # and the specified position."""
+        if self.__size == 0:
+            print()
+            return
+
+        for _ in range(self.__position[1]):
+            print()
+
+        for _ in range(self.__size):
+            print(" " * self.__position[0] + "#" * self.__size)
