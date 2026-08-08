dbfff7fc0802f8f99cfdb01daf8e29cff0b37dc9
Re-joice <ofosuarmahrejoice@gmail.com>
2026-08-08 06:18:52 -0700

    Update rectangle display for x and y

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -77,8 +77,10 @@ class Rectangle(Base):
 
     def display(self):
         """Print the rectangle with the # character."""
+        for _ in range(self.__y):
+            print()
         for _ in range(self.__height):
-            print("#" * self.__width)
+            print(" " * self.__x + "#" * self.__width)
 
     def __str__(self):
         """Return the string representation of the rectangle."""
