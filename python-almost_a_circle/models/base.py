7ae40534f6fcd935b563c705c2f55132cb4f8e3b
Re-joice <ofosuarmahrejoice@gmail.com>
2026-08-08 05:27:10 -0700

    Implement Base class

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -1,11 +1,9 @@
 #!/usr/bin/python3
 """Defines the Base class."""
 
-import json
-
 
 class Base:
-    """Base class for the project."""
+    """Base class for managing id."""
 
     __nb_objects = 0
 
@@ -16,17 +14,3 @@ class Base:
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
-
-    @staticmethod
-    def to_json_string(list_dictionaries):
-        """Return the JSON representation of a list of dictionaries."""
-        if list_dictionaries is None or len(list_dictionaries) == 0:
-            return "[]"
-        return json.dumps(list_dictionaries)
-
-    @staticmethod
-    def from_json_string(json_string):
-        """Return the Python representation of a JSON string."""
-        if json_string is None or json_string == "":
-            return []
-        return json.loads(json_string)
