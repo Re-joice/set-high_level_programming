7f699d7087a8e0d3fe24e71eccb7dbde528028b8
Re-joice <ofosuarmahrejoice@gmail.com>
2026-08-08 07:04:34 -0700

    Add Base JSON string method

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
 """Defines the Base class."""
 
+import json
+
 
 class Base:
-    """Base class for managing id."""
+    """Base class for all other classes."""
 
     __nb_objects = 0
 
@@ -14,3 +16,10 @@ class Base:
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        """Return the JSON string representation of a list of dictionaries."""
+        if list_dictionaries is None or len(list_dictionaries) == 0:
+            return "[]"
+        return json.dumps(list_dictionaries)
