3cdb9d2c5b07c2b53e7c5095d468874f314aa413
Re-joice <ofosuarmahrejoice@gmail.com>
2026-08-08 07:20:31 -0700

    Add Base load from file method

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -56,3 +56,18 @@ class Base:
 
         dummy.update(**dictionary)
         return dummy
+
+    @classmethod
+    def load_from_file(cls):
+        """Return a list of instances from a JSON file."""
+        filename = cls.__name__ + ".json"
+
+        try:
+            with open(filename, "r") as file:
+                json_string = file.read()
+        except FileNotFoundError:
+            return []
+
+        list_dictionaries = cls.from_json_string(json_string)
+
+        return [cls.create(**dictionary) for dictionary in list_dictionaries]
