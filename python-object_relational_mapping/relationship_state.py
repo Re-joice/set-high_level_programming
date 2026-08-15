d5fa99bd907a5661d61ab948d8905b29db11ea34
Rejoice Ofosuarmah <ofosuarmahrejoice@example.com>
2026-08-15 10:01:08 -0700

    Add State and City relationship

---
+++ python-object_relational_mapping/relationship_state.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""State module."""
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.orm import relationship
+from model_state import Base
+
+
+class State(Base):
+    """State class."""
+    __tablename__ = "states"
+
+    id = Column(Integer, primary_key=True, nullable=False)
+    name = Column(String(128), nullable=False)
+
+    cities = relationship(
+        "City",
+        back_populates="state",
+        cascade="all, delete-orphan"
+    )
