769935b218489d05d3f8e06ae3be2d40a98aedf0
Rejoice Ofosuarmah <ofosuarmahrejoice@example.com>
2026-08-15 10:24:45 -0700

    Add script to list states and cities

---
+++ python-object_relational_mapping/101-relationship_states_cities_list.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""List all states and their cities."""
+import sys
+
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, joinedload
+
+from relationship_state import State
+from relationship_city import City
+
+
+if __name__ == "__main__":
+    engine = create_engine(
+        "mysql+mysqldb://{}:{}@localhost/{}".format(
+            sys.argv[1], sys.argv[2], sys.argv[3]
+        ),
+        pool_pre_ping=True
+    )
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    states = session.query(State).options(
+        joinedload(State.cities)
+    ).order_by(State.id).all()
+
+    for state in states:
+        print("{}: {}".format(state.id, state.name))
+        for city in sorted(state.cities, key=lambda city: city.id):
+            print("\t{}: {}".format(city.id, city.name))
+
+    session.close()
