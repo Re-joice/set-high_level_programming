89c1bf9ba8f1afae300816dfbf1732d2baaacc55
Rejoice Ofosuarmah <ofosuarmahrejoice@example.com>
2026-08-22 03:08:42 -0700

    Add HTTP error handling script

---
+++ python-network_1/3-error_code.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Fetch a URL and handle HTTP errors."""
+
+from sys import argv
+from urllib import error, request
+
+
+if __name__ == "__main__":
+    try:
+        with request.urlopen(argv[1]) as response:
+            print(response.read().decode("utf-8"))
+    except error.HTTPError as e:
+        print("Error code:", e.code)
