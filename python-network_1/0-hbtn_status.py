9bd737067cca66489568ab9999a90cf234ad8235
Rejoice Ofosuarmah <ofosuarmahrejoice@example.com>
2026-08-22 02:22:42 -0700

    Add hbtn status script

---
+++ python-network_1/0-hbtn_status.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Fetch and display the status of the ALX intranet webpage."""
+
+from urllib import request
+
+
+if __name__ == "__main__":
+    with request.urlopen("https://alx-intranet.hbtn.io/status") as response:
+        body = response.read()
+        print("Body response:")
+        print("\t- type:", type(body))
+        print("\t- content:", body)
+        print("\t- utf8 content:", body.decode("utf-8"))
---
+++ python-network_1/README.md
@@ -0,0 +1,3 @@
+# Python - Network #1
+
+This project contains Python scripts that use urllib to communicate with web servers.
