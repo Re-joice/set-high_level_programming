28afc588ac51ddd4d0ccda038c13517e11d5328c
Rejoice Ofosuarmah <ofosuarmahrejoice@example.com>
2026-08-22 01:59:48 -0700

    Fix catch me request method

--- python-network_0/102-catch_me.sh
+++ python-network_0/102-catch_me.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# Sends a request to the catch_me endpoint and follows the server redirect.
-curl -sL 0.0.0.0:5000/catch_me
+# Sends a PUT request to the catch_me endpoint and displays the response body.
+curl -s -X PUT 0.0.0.0:5000/catch_me
