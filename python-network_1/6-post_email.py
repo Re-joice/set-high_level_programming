4d38aa5377e1613a23bd94d9f388086dc51de9e5
Rejoice Ofosuarmah <ofosuarmahrejoice@example.com>
2026-08-22 03:36:52 -0700

    Add requests POST email script

---
+++ python-network_1/6-post_email.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Send an email as a POST parameter and display the response body."""
+
+import requests
+import sys
+
+
+if __name__ == "__main__":
+    response = requests.post(
+        sys.argv[1],
+        data={"email": sys.argv[2]}
+    )
+    print(response.text)
